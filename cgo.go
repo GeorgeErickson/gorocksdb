a3a45057ccc84e41e4ef68a32cc74fae7f30a6ce
George Erickson <me@gee.io>
2018-10-01 21:36:21 -0400

    fixup! wip

--- cgo.go
+++ cgo.go
@@ -1,8 +1,11 @@
 package gorocksdb
 
-// #cgo CPPFLAGS: -I${SRCDIR}/deps/rocksdb/include -I${SRCDIR}/deps/rocksdb
+// #cgo CPPFLAGS: -I${SRCDIR}/deps/rocksdb/include -I${SRCDIR}/deps/rocksdb -DNDEBUG -DROCKSDB_PLATFORM_POSIX -DROCKSDB_LIB_IO_POSIX
+// #cgo darwin CPPFLAGS: -DOS_MACOSX -DROCKSDB_BACKTRACE
+// #cgo linux CPPFLAGS: -DOS_LINUX -fno-builtin-memcmp -DROCKSDB_MALLOC_USABLE_SIZE
 // #cgo CXXFLAGS: -std=c++11
 // #cgo LDFLAGS: -lrocksdb
 // #cgo LDFLAGS: -lsnappy -llz4 -lzstd
 // #cgo LDFLAGS: -lstdc++ -lm
+// #cgo linux LDFLAGS: -lrt
 import "C"
